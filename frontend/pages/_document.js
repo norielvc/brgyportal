db45ad18dbd658cdbedd417cc2b53a7b0e132396
Iba O' Este Admin <admin@ibaoeste.com>
2026-03-22 13:53:13 +0800

    fix(layout): force 75% zoom default view bypass production css compiler stripping

--- frontend/pages/_document.js
+++ frontend/pages/_document.js
@@ -2,7 +2,7 @@ import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
-    <Html lang="en">
+    <Html lang="en" style={{ zoom: '0.75' }}>
       <Head>
         {/* CSP is handled by the backend server */}
         <link rel="icon" href="/favicon.ico" />
